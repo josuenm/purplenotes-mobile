f245365e7bdfac9bdaa5e4448bb00f137461c97e
josuenm <direct.josue@gmail.com>
2022-08-18 15:33:05 -0300

    Context API Provider, StatusBar configurado

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import { StatusBar } from "native-base";
 import React from "react";
 import AppContainer from "./src/components/AppContainer";
 import AppNavigation from "./src/components/AppNavigation";
+import { StatusBar } from "native-base";
+import { GlobalToolsContextProvider } from "./src/contexts/globalToolsContext";
+import { UserContextProvider } from "./src/contexts/userContext";
 
 export default function App() {
   return (
     <AppContainer>
-      <StatusBar translucent />
-      <AppNavigation />
+      <GlobalToolsContextProvider>
+        <UserContextProvider>
+          <StatusBar
+            translucent
+            barStyle="dark-content"
+            backgroundColor="rgba(238, 238, 238, .6)"
+          />
+          <AppNavigation />
+        </UserContextProvider>
+      </GlobalToolsContextProvider>
     </AppContainer>
   );
 }
