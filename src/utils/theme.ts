e97553993a844a9fb3259ec361af94b0c3a61384
josuenm <direct.josue@gmail.com>
2022-08-19 19:04:55 -0300

    Estilzação global dos elementos do native base

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,8 +1,43 @@
 import { extendTheme } from "native-base";
 
-const config = {
+export default extendTheme({
   useSystemColorMode: false,
   initialColorMode: "light",
-};
+  fontConfig: {
+    Roboto: {
+      300: {
+        normal: "Roboto_300Light",
+      },
+      500: {
+        normal: "Roboto_500Medium",
+      },
+      700: {
+        normal: "Roboto_700Bold",
+      },
+    },
+  },
 
-export default extendTheme({ config });
+  components: {
+    Link: {
+      baseStyle: () => {
+        return { _text: { color: "violet.600" } };
+      },
+    },
+    Text: {
+      baseStyle: () => {
+        return { fontWeight: 300 };
+      },
+    },
+    Heading: {
+      baseStyle: () => {
+        return { fontWeight: 700 };
+      },
+    },
+  },
+
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+    mono: "Roboto",
+  },
+});
