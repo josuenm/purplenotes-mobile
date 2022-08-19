bceac993d960a0ce7304f31483c6e3ae19a94718
josuenm <direct.josue@gmail.com>
2022-08-19 19:10:52 -0300

    Botões estilizados e links colocados

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,56 +1,71 @@
-import { AntDesign, Ionicons, Octicons } from "@expo/vector-icons";
-import { Box, Flex, IconButton, Text } from "native-base";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { useRoute } from "@react-navigation/native";
+import { Box, IconButton, Text } from "native-base";
 import { TouchableOpacity } from "react-native";
+import * as RootNavigation from "../utils/RootNavigation";
 
 interface ItemProps {
   title: string;
-  children: React.ReactNode;
+  routeName: string;
+  iconName: string;
 }
 
-function Item({ title, children }: ItemProps) {
+function Item({ title, routeName, iconName }: ItemProps) {
+  const route = useRoute();
+
+  const isActive = routeName === route.name;
+
+  function Navigate() {
+    RootNavigation.navigate(routeName);
+  }
+
   return (
-    <TouchableOpacity activeOpacity={0.3}>
-      <Box alignItems="center" px={2}>
-        {children}
-        <Text>{title}</Text>
+    <TouchableOpacity activeOpacity={0.3} onPress={Navigate}>
+      <Box alignItems="center" px={3}>
+        <Ionicons
+          name={iconName as any}
+          size={20}
+          color={isActive ? "#7c3aed" : "black"}
+        />
+        <Text color={isActive ? "#7c3aed" : "black"}>{title}</Text>
       </Box>
     </TouchableOpacity>
   );
 }
 
+function AddButton() {
+  return (
+    <IconButton
+      borderRadius="full"
+      bgColor="violet.600"
+      icon={<AntDesign name="plus" size={24} color="white" />}
+      p={3}
+      translateY={-20}
+      _pressed={{ opacity: 0.7 }}
+    />
+  );
+}
+
 export default function BottomNavigation() {
   return (
-    <Flex
-      w="full"
-      px={5}
-      py={1}
+    <Box
       position="absolute"
       bottom={0}
-      direction="row"
-      justifyContent="space-between"
-      bgColor="rgba(238, 238, 238, .8)"
+      w="full"
       borderTopWidth={1}
       borderTopColor="#ddd"
+      flexDirection="row"
+      justifyContent="space-between"
+      px={5}
+      py={1}
     >
-      <Item title="Home">
-        <Octicons name="home" size={24} color="black" />
-      </Item>
-
-      <IconButton
-        borderRadius="full"
-        bgColor="violet.500"
-        justifyContent="center"
-        p={3}
-        style={{ transform: [{ translateY: -20 }] }}
-        _pressed={{
-          opacity: 0.7,
-        }}
-        icon={<AntDesign name="plus" size={24} color="white" />}
+      <Item title="Home" routeName="Dashboard" iconName="home-outline" />
+      <AddButton />
+      <Item
+        title="Settings"
+        routeName="Settings"
+        iconName="md-settings-outline"
       />
-
-      <Item title="Settings">
-        <Ionicons name="settings-outline" size={24} color="black" />
-      </Item>
-    </Flex>
+    </Box>
   );
 }
