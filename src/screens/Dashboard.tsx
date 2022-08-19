febc354896f3d840de7557a3519de3b82adfc31a
josuenm <direct.josue@gmail.com>
2022-08-19 19:08:48 -0300

    useEffect para listar as anotações, fragmento em volta da aplicação

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,26 +1,34 @@
-import { Box, ScrollView, Text } from "native-base";
-import { useContext } from "react";
+import { Box, Heading, ScrollView, Text } from "native-base";
+import { useContext, useEffect } from "react";
+import BottomNavigation from "../components/BottomNavigation";
 import { NotesContext, NotesContextProps } from "../contexts/notesContext";
 
 export default function Dashboard() {
   const { List, notes } = useContext(NotesContext) as NotesContextProps;
 
+  useEffect(() => {
+    List();
+  }, []);
+
   return (
-    <ScrollView bgColor="#eee">
-      <Box pt={20} px={5}>
-        <Text fontSize={21} fontWeight="bold">
-          Notes:
-        </Text>
-        {notes.length > 0 ? (
-          <Text fontSize={21} textAlign="center" fontWeight="medium">
-            List
-          </Text>
-        ) : (
-          <Text fontSize={21} textAlign="center" fontWeight="medium">
-            Nothing to list
-          </Text>
-        )}
-      </Box>
-    </ScrollView>
+    <>
+      <ScrollView bgColor="#eee">
+        <Box pt={20} px={5}>
+          <Heading fontSize={21} fontWeight={700}>
+            Notes:
+          </Heading>
+          {notes.length > 0 ? (
+            <Text fontSize={21} textAlign="center" fontWeight={500}>
+              List
+            </Text>
+          ) : (
+            <Text fontSize={21} textAlign="center" fontWeight={500}>
+              Nothing to list
+            </Text>
+          )}
+        </Box>
+      </ScrollView>
+      <BottomNavigation />
+    </>
   );
 }
