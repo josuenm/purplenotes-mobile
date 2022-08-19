aaf6cedcd3f8fdb8b966b15236c36b817cdad761
josuenm <direct.josue@gmail.com>
2022-08-19 19:03:21 -0300

    Cabeçalho com botão de voltar

---
+++ src/components/HeaderWithBackButton.tsx
@@ -0,0 +1,37 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Box, Button } from "native-base";
+import * as RootNavigation from "../utils/RootNavigation";
+
+interface HeaderProps {
+  returnTo: string;
+}
+
+export default function HeaderWithBackButton({ returnTo }: HeaderProps) {
+  function Navigate() {
+    RootNavigation.navigate(returnTo);
+  }
+
+  return (
+    <Box
+      w="full"
+      flexDirection="row"
+      justifyContent="flex-start"
+      alignItems="center"
+      pt={34}
+      px={5}
+      bgColor="#eee"
+    >
+      <Button
+        w={42}
+        variant="unstyled"
+        borderRadius="full"
+        _pressed={{
+          bgColor: "rgba(218,218,218,.5)",
+        }}
+        onPress={Navigate}
+      >
+        <MaterialIcons name="arrow-back-ios" size={27} color="black" />
+      </Button>
+    </Box>
+  );
+}
