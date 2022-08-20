4aeb2d9e519469d0801e5a28fcc0b7d32431fe29
josuenm <direct.josue@gmail.com>
2022-08-20 18:36:52 -0300

    Tipagens relacionadas ao usuário

---
+++ src/types/UserProps.ts
@@ -0,0 +1,15 @@
+export interface UserProps {
+  name: string;
+  email: string;
+}
+
+export interface LoginProps {
+  email: string;
+  password: string;
+}
+
+export interface RegisterProps {
+  name: string;
+  email: string;
+  password: string;
+}
